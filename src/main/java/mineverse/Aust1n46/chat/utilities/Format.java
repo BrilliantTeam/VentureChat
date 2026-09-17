84b608a6aff3aefea35a71eab6ff963e028277e6
RICE0707 <master.end.9377@gmail.com>
2026-09-17 12:35:15 +0800

    修復錯誤

--- src/main/java/mineverse/Aust1n46/chat/utilities/Format.java
+++ src/main/java/mineverse/Aust1n46/chat/utilities/Format.java
@@ -1046,11 +1046,12 @@ public class Format {
 	}
 	
 	private static Sound getSound(String soundName) {
-		if(Arrays.asList(Sound.values()).stream().map(Sound::toString).collect(Collectors.toList()).contains(soundName)) {
+		try {
 			return Sound.valueOf(soundName);
+		} catch (RuntimeException e) {
+			Bukkit.getConsoleSender().sendMessage(Format.FormatStringAll("&8[&eVentureChat&8]&c - Message sound invalid!"));
+			return getDefaultMessageSound();
 		}
-		Bukkit.getConsoleSender().sendMessage(Format.FormatStringAll("&8[&eVentureChat&8]&c - Message sound invalid!"));
-		return getDefaultMessageSound();
 	}
 	
 	private static Sound getDefaultMessageSound() {
